674cdbf52a90ff10b396b3029fbd316ee45dfd70
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2022-08-23 15:21:47 -0400

    fix: change the color of the title text

--- src/components/CarouselHome/index.js
+++ src/components/CarouselHome/index.js
@@ -2,6 +2,7 @@ import { Carousel, Image, Typography } from "antd";
 import React from "react";
 import homebg_1 from "../../assets/homebg_1.png";
 import homebg_2 from "../../assets/homebg_2.png";
+import homebg_3 from "../../assets/homebg_3.png";
 import "./styles.css";
 
 const contentStyle = {
@@ -14,17 +15,39 @@ const contentStyle = {
   backgroundSize: "cover",
 };
 
-const WinterRoadTitle = () => (
-  <div style={{ marginTop: "-430px" }} className="titleText">
-    <Typography style={{ color: "white", fontSize: "8vh" }}>WRTDIP</Typography>
-    <Typography
-      style={{ color: "white", fontSize: "6vh" }}
-      className="titleText"
-    >
-      Winter Road and Trail Data Information Portal{" "}
-    </Typography>
-  </div>
-);
+const WinterRoadTitle_White = () => {
+  return (
+    <div style={{ marginTop: "-430px" }} className="titleText">
+      <Typography style={{ color: "white", fontSize: "8vh" }}>
+        WRTDIP
+      </Typography>
+      <Typography
+        style={{ color: "white", fontSize: "6vh" }}
+        className="titleText"
+      >
+        Winter Road and Trail Data Information Portal{" "}
+      </Typography>
+    </div>
+  );
+};
+
+// The reason why set this separate is becuase the position of the title might need to be adjusted in the future
+// This will make everything more maintainable
+const WinterRoadTitle_Black = () => {
+  return (
+    <div style={{ marginTop: "-430px" }} className="titleText">
+      <Typography style={{ color: "black", fontSize: "8vh" }}>
+        WRTDIP
+      </Typography>
+      <Typography
+        style={{ color: "black", fontSize: "6vh" }}
+        className="titleText"
+      >
+        Winter Road and Trail Data Information Portal{" "}
+      </Typography>
+    </div>
+  );
+};
 
 const BackGroundImageOne = () => (
   <Image
@@ -45,17 +68,17 @@ const CarouselHome = () => (
           backgroundImage: `url(${homebg_1})`,
         }}
       >
-        <WinterRoadTitle />
+        <WinterRoadTitle_White />
       </div>
     </div>
     <div>
       <div style={{ ...contentStyle, backgroundImage: `url(${homebg_2})` }}>
-        <WinterRoadTitle />
+        <WinterRoadTitle_White />
       </div>
     </div>
     <div>
-      <div style={{ ...contentStyle, backgroundImage: `url(${homebg_2})` }}>
-        <WinterRoadTitle />
+      <div style={{ ...contentStyle, backgroundImage: `url(${homebg_3})` }}>
+        <WinterRoadTitle_Black />
       </div>
     </div>
   </Carousel>
