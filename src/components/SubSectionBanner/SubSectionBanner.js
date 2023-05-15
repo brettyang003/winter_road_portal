fd325d5fda5f74334be2b1dbde370ea07214c723
brettyang003 <brettyang78@gmail.com>
2023-05-15 10:22:16 -0400

    change

---
+++ src/components/SubSectionBanner/SubSectionBanner.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Grid, Layout, Menu, Typography } from "antd";
+import { Link } from "react-router-dom";
+import CanadaMap from "../../assets/canada_map.png";
+import { MenuOutlined } from "@ant-design/icons";
+import titleImage from "../../assets/homebg_3.png";
+import "./styles.css";
+
+const SubSectionBanner = ({ props }) => {
+  const { image, title, content, bgColor } = props;
+  return (
+    <div
+      className="subSectionBannerContainer"
+      style={{ backgroundColor: bgColor }}
+    >
+      <div className="subSectionBannerTextContainer">
+      <div className="subSectionBannerTitle">
+        {title}
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubSectionBanner;
